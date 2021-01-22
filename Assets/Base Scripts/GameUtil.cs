e7d701a3f3f3b58562fcfdbc83a00d976f5bd984
Jackson Dean <developer@jacksonsdean.com>
2021-01-22 10:15:53 -0500

    add 2d sprite package and game util class

---
+++ Assets/Base Scripts/GameUtil.cs
@@ -0,0 +1,10 @@
+﻿using DG.Tweening;
+public class GameUtil
+{
+
+    public static void WaitThenDo(float seconds, TweenCallback callback)
+    {
+        float t = 0;
+        DOTween.To(() => t, (x) => t = x, seconds, seconds).OnComplete(callback);
+    }
+}
